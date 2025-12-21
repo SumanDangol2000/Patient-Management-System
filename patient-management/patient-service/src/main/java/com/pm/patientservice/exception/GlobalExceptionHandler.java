9a83de6052b681b58ec6ddc60d3dd25460d11da0
Suman Dangol <sdangol368@gmail.com>
2025-12-21 14:04:32 -0600

    patient-service : define email specific exception handling, check if the email already exist and handle the error

--- patient-management/patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-management/patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.pm.patientservice.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +13,8 @@ import java.util.Map;
 @ControllerAdvice //centralize the exception handling
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class) //catches/hanldes all exceptions/errors raised from JPA (@NotBlank and so on)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex){
         Map<String, String> errors = new HashMap<>();
@@ -20,5 +24,16 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.badRequest().body(errors);
     }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleEmailAlreadyExistException(EmailAlreadyExistsException ex){
+
+        log.warn("Email address already exist : {}", ex.getMessage());
+
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Email address already exists.");
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 
 }
